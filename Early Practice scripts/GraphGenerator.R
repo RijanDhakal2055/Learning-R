1ccf0c573a881eda5916835c3e51a4447d8193fb
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-05-13 18:18:26 -0500

    the one that takes in clarity and fill both

--- Early Practice scripts/GraphGenerator.R
+++ Early Practice scripts/GraphGenerator.R
@@ -1,4 +1,12 @@
 packageVersion("tidyverse")
 library("tidyverse")
-ggplot(data = diamonds) +
-	geom_bar(mapping = aes(x= cut, color = clarity))
+ggplot(
+	data = diamonds,
+	mapping = aes(x =cut, fill = clarity)
+) +
+	geom_bar(alpha =1/5, position = "identity")
+ggplot(
+	data = diamonds,
+	mapping= aes(x = cut, color = clarity)
+)+
+	geom_bar(fill= NA, position = "identity")
