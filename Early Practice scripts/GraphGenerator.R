3311a7882ebfb196c6e476289bbee7545753e241
NoobyMcNoobNoobson <dhakalrijan825@gmail.com>
2021-05-13 22:37:47 -0500

    position dodge

--- Early Practice scripts/GraphGenerator.R
+++ Early Practice scripts/GraphGenerator.R
@@ -1,12 +1,7 @@
 packageVersion("tidyverse")
 library("tidyverse")
-ggplot(
-	data = diamonds,
-	mapping = aes(x =cut, fill = clarity)
-) +
-	geom_bar(alpha =1/5, position = "identity")
-ggplot(
-	data = diamonds,
-	mapping= aes(x = cut, color = clarity)
-)+
-	geom_bar(fill= NA, position = "identity")
+ggplot( data = diamonds) +
+	geom_bar(
+		mapping = aes(x =cut, fill = clarity),
+		position = "dodge"
+	)
